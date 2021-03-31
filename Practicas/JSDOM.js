ad603fdfa7e661722b9f66579c5eede5a3c70d86
Antonio <antonio.enralv@gmail.com>
2021-03-31 11:48:50 -0600

    Tarea de table mediante JS

--- Practicas/JSDOM.js
+++ Practicas/JSDOM.js
@@ -1,5 +1,4 @@
-/* 
-  Ejercicio 3: 
+/*  Ejercicio 3: 
     1.- Vamos a utilizar el arreglo de objetos para crear una tabla 
     2.- Crear la tabla utilizando los metodos de "document" 
     3.- Iterar sobre el arreglo para crear headers 
@@ -18,8 +17,43 @@ var data = [
     {name: "Popocatepetl", height: 5465, place: "Mexico"}, 
     {name: "Mont Blanc", height: 4808, place: "Italy/France"} 
   ];
-
 //Se crea la función
-  const createTable = () => {
+const createTable = (inputArray) => {
+    const container = document.getElementById("container");
+    const table = document.createElement("table"); // Representa <table></table> en el DOM
+    const tableHead = document.createElement("thead");
+    const fields = Object.keys(inputArray[0]) //Nos trae: [name, height, place]
 
+    fields.forEach((field) =>{
+        //La primera vez tiene name, la segunda height, la tercera place
+        const headCell = document.createElement("th"); // <th></th>
+        headCell.appendChild(document.createTextNode(field));
+        tableHead.appendChild(headCell); //<th>name</th> ===> <thead> <th>name</th> </thead>
+    });
+    table.appendChild(tableHead);
+    container.appendChild(table);
+
+    const body = document.createElement("tbody"); //  <tbody></tbody>
+
+    inputArray.forEach((obj) => {
+        //1era vez: obj= {name: "Kilimanjaro", height: 5895, place: "Tanzania"}
+        const row = document.createElement("tr"); // <tr></tr> 
+
+        fields.forEach((field)=>{
+            const cell = document.createElement("td");
+            cell.appendChild(document.createTextNode(obj[field])); // <td>Kilimanjaro</td>
+            row.appendChild(cell); //<tr> <td>Kilimanjaro</td> </tr>
+        })
+
+        body.appendChild(row);
+    })
+
+    table.appendChild(body);
+
+    //Estilos
+    table.setAttribute("border", "5px");
+    table.style.border="1px solid yellow";
+    table.style.textTransform="capitalize";
 }
+//Se manda a llamar la función 
+createTable(data);
